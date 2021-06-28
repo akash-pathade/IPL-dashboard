de3fbf40a87ab860077e96d61837003ec8800bd9
Akash Pathade <akash.pathade@sap.com>
2021-06-28 17:58:41 +0530

    5. Populated teams data from csv and saved to in mem DB

---
+++ src/main/java/com/akash/dashboardservice/model/Team.java
@@ -0,0 +1,33 @@
+package com.akash.dashboardservice.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public Team(String teamName, long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
+    @Override
+    public String toString() {
+        return "Team [teamName=" + teamName + ", totalMatches=" + totalMatches + ", totalWins=" + totalWins + "]";
+    }
+    public Team() {
+
+    }
+
+}
